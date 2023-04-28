4638bc5f7d80bd29ecf7e7c3841a3af3b30a0daf
peazedev <peazedev@users.noreply.github.com>
2023-04-28 13:26:13 -0400

    chore: Update contract address

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CHAIN_ID = 80001;
-export const CONTRACT_ADDRESS = "0x376543572a5A6B69F38cdcc188CAB7Cad3B7E26f";
-export const CONTRACT_DEPLOY_BLOCK = 33512407;
+export const CONTRACT_ADDRESS = "0x28760b431fb71594843970Cf61fC745661B2B8B8";
+export const CONTRACT_DEPLOY_BLOCK = 34940133;
 export const EXPLORER_URL =
   CHAIN_ID === 80001
     ? "https://mumbai.polygonscan.com"
